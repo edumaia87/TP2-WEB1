270c6c0a3e0352f0353894562d5282edeaaa24e6
Eduardo Maia <edumaia89@outlook.com>
2022-11-06 10:38:01 -0300

    iniciando o carrinho

--- views/cart.php
+++ views/cart.php
@@ -7,6 +7,52 @@
     <title>Carrinho</title>
 </head>
 <body>
-    <h1>Tela do carrinho</h1>
+    <?php include 'header.php'; ?>
+    
+    <h1 class="my-cart"><i class="fa fa-shopping-cart"></i> Meu carrinho</h1>
+    <section class="cart-content">
+        <div class="cart-products">
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Produto</th>
+                        <th>Qtd</th>
+                        <th>Preço</th>
+                        <th>Excluir</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th><a href="#envia-pro-produto"><img src="../img/led-zeppeling-icon.jpg" alt=""></a></th>
+                        <td>Livro Led Zeppelin</td>
+                        <td>1</td>
+                        <td>1 x R$ 199.90</td>
+                        <td><a href="#"><i class="fa fa-trash"></i></a></td>
+                    </tr>
+                </tbody>
+            </table>
+            <button id="clear-cart" type="submit">Limpar carrinho</button>
+        </div>
+
+        <div class="shipping-discount">
+            <div class="discount-coupon">
+                <h3>Cupom de desconto:</h3>
+                <div>
+                    <input type="text" placeholder="Cupom">
+                    <button><i class="fa fa-ticket"></i> Aplicar</button>
+                </div>
+            </div>
+            <div class="shipping">
+            <h3>Frete e prazos:</h3>
+                <div>
+                    <input type="text" placeholder="CEP*">
+                    <button><i class="fa fa-truck-fast"></i> Calcular</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include 'footer.php'; ?>
 </body>
 </html>
